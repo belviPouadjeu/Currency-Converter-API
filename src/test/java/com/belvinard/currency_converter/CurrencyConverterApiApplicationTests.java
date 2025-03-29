77fdb28383fd61d10e712d071ec72fa5915bd1dd
belviPouadjeu <pbelvinard21@gmail.com>
2025-03-29 17:29:24 +0100

    Initialize project

---
+++ src/test/java/com/belvinard/currency_converter/CurrencyConverterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belvinard.currency_converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
